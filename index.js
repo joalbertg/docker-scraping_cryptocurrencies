cde23f9fab1133889e2de113a4e878ec498f001a
Joalbert <joalbertgonzalez@gmail.com>
2020-04-15 18:16:10 -0500

    scraping cryptocurrencies

--- index.js
+++ index.js
@@ -1,2 +1,26 @@
-console.log('Hello world');
+const cheerio = require('cheerio'),
+  fetch = require('node-fetch');
+
+async function main() {
+  let response = await fetch('https://mx.investing.com/crypto'),
+      html = await response.text();
+
+  let $ = cheerio.load(html),
+      cryptos = [],
+      crypto,
+      tbody = $('tbody').find('tr').toArray();
+
+  for(let tr of tbody) {
+    crypto = [];
+    tr = $(tr).find('td').filter((i, elem) => $(elem).text()).toArray();
+
+    for(let td of tr) {
+      crypto.push($(td).text());
+    }
+    cryptos.push(crypto);
+  }
+  console.table(cryptos);
+}
+
+setInterval(() => main(), 1000);
 
